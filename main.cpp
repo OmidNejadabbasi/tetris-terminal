e36b6fd902e57be30e9c12be714096670847053d
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-08 18:44:39 +0330

    adding tetrominos but only the first one is correct

--- main.cpp
+++ main.cpp
@@ -2,9 +2,52 @@
 
 using namespace std;
 
+wstring tetrominos[7];
 
 int main(int argc, const char **argv)
 {
+
+    // Create assets 
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+
+    
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
+    tetrominos[0].append(L"..X.");
 
     return 0;
 }
