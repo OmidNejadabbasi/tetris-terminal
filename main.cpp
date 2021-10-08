bb5203220c407d230e5698ab4de639d451ef40fb
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-08 18:49:16 +0330

    correct the other shapes

--- main.cpp
+++ main.cpp
@@ -14,40 +14,40 @@ int main(int argc, const char **argv)
     tetrominos[0].append(L"..X.");
 
 
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+    tetrominos[1].append(L"..X.");
+    tetrominos[1].append(L".XX.");
+    tetrominos[1].append(L".X..");
+    tetrominos[1].append(L"....");
 
 
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+    tetrominos[2].append(L".X..");
+    tetrominos[2].append(L".XX.");
+    tetrominos[2].append(L"..X.");
+    tetrominos[2].append(L"....");
 
 
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+    tetrominos[3].append(L"....");
+    tetrominos[3].append(L".XX.");
+    tetrominos[3].append(L".XX.");
+    tetrominos[3].append(L"....");
 
 
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+    tetrominos[4].append(L"..X.");
+    tetrominos[4].append(L".XX.");
+    tetrominos[4].append(L"..X.");
+    tetrominos[4].append(L"....");
 
 
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+    tetrominos[5].append(L"....");
+    tetrominos[5].append(L".XX.");
+    tetrominos[5].append(L"..X.");
+    tetrominos[5].append(L"..X.");
 
-    
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
-    tetrominos[0].append(L"..X.");
+
+    tetrominos[6].append(L"....");
+    tetrominos[6].append(L"..XX");
+    tetrominos[6].append(L"..X.");
+    tetrominos[6].append(L"..X.");
 
     return 0;
 }
