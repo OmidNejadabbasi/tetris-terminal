c9bcbd7a14f728ec3a7ad26badd3dccad9ca1195
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-12 13:55:32 +0300

    define DoesPieceFit function

--- main.cpp
+++ main.cpp
@@ -26,6 +26,30 @@ int Rotate(int px, int py, int r)
     return 0;
 }
 
+bool DoesPieceFit(int idTetromino, int rotation, int posX, int posY)
+{
+    for (size_t x = 0; x < 4; x++)
+        for (int y = 0; y < 4; y++)
+        {
+            int pi = Rotate(x, y, rotation);
+
+            int fi = (posY + y) * fieldWidth + posX + x;
+
+            if (posX + x >= 0 && posX + x < fieldWidth)
+            {
+                if (posY + y < fieldHeight && posY + y >= 0)
+                {
+                    if (tetrominos[idTetromino][pi] == L"X" && pField[fi] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+    return true;
+}
+
 int main(int argc, const char **argv)
 {
 
@@ -84,6 +108,14 @@ int main(int argc, const char **argv)
     while (!gameOver)
     {
 
+        // GAME TIMING =======================================
+
+        // INPUT =============================================
+
+        // GAME LOGIC ========================================
+
+        // RENDER ============================================
+
         // Draw the screen
         for (int x = 0; x < fieldWidth; x++)
         {
@@ -92,7 +124,7 @@ int main(int argc, const char **argv)
                 screen[(y + 2) * nScreenWidth + x + 2] = L" ▤▩▧▣▤▧▣=#"[pField[y * fieldWidth + x]];
             }
         }
-        std::cin >> nScreenHeight;
+
         // Disply console frame
         WriteConsoleOutputCharacterW(Console, screen, nScreenHeight * nScreenWidth, {0, 0}, &dwBytesWritten);
     }
